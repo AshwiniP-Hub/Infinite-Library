06635dd267580392a22ed7424616cd1a169a0ba4
AshwiniP-Hub <ashwinivpatil4@gmail.com>
2025-01-29 17:48:40 +0530

    Update Cart.jsx

--- Frontend/src/components/Cart.jsx
+++ Frontend/src/components/Cart.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import cart from "../../public/Images/cart.png";
 
 const Cart = () => {
   return (
     <>
     <div className='flex flex-col justify-center items-center'>
         <div>
-        <img src='./Images/cart.png' alt="error" className='size-48 mt-4' />
+        <img src={cart} alt="error" className='size-48 mt-4' />
         </div>
         <div>
         <h1 className='text-xl mt-4'>Your Cart is empty. Browse now and pick something you love!</h1>
